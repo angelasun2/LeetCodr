a67534c27316c1f0c1fb7cd7aab6dd6d6365bc6a
Angela Sun <35405157+angelasun2@users.noreply.github.com>
2018-12-20 18:23:41 -0800

    misc

---
+++ precache-manifest.14ed0ffac6941e278d913a49416b5f09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9ac9281dfe79566e6c5a",
+    "url": "/static/js/main.9ac9281d.chunk.js"
+  },
+  {
+    "revision": "f48a71497e657533950b",
+    "url": "/static/js/1.f48a7149.chunk.js"
+  },
+  {
+    "revision": "9ac9281dfe79566e6c5a",
+    "url": "/static/css/main.21b34756.chunk.css"
+  },
+  {
+    "revision": "4d3ec4a9f304aefb03d9e5b3abc7a8ce",
+    "url": "/index.html"
+  }
+];
